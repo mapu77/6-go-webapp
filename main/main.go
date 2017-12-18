81285c9512513da3eba12001c342daf985bf6d92
Eduard Maura <edu.maura.puig@gmail.com>
2017-12-19 00:57:16 +0100

    Extract port to env variable for heroku deployment

--- main/main.go
+++ main/main.go
@@ -5,13 +5,17 @@ import (
 	"log"
 	"github.com/gorilla/sessions"
 	"github.com/mapu77/AD-Labs/6-go-webapp/routes"
+	"os"
 )
 
 func main() {
-
+	port, exists := os.LookupEnv("PORT")
+	if !exists {
+		port = "8080"
+	}
 	sessions.NewCookieStore([]byte("travelAgencyCookie"))
 	router := routes.GetRouter()
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 
 }
